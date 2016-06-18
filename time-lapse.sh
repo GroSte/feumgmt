dc11a0b57078dc11f56d4a41ec1b36ff80624b42
steve <stevegr@web.de>
2016-06-18 18:10:07 +0200

    tuned up webcam settings

--- time-lapse.sh
+++ time-lapse.sh
@@ -10,4 +10,4 @@ filename=/usr/local/media/images/$timestamp.jpg
 #Foto erstellen
 echo $filename
 
-fswebcam --no-banner -r 640x480 -d /dev/video0 -i 0 $filename
+fswebcam --no-banner -r 640x480 -d /dev/video0 -i 0 -F 15 -D 4 -S 40 --jpeg 95 $filename
