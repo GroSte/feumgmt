e7a397fbd38496d446b036b3f7962c99b84fa90d
GroSte <computer0205>
2016-06-20 09:16:31 +0200

    added vehicles

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
-from base.models import Mission
+from base.models import Mission, Vehicle
 
 admin.site.register(Mission, LeafletGeoAdmin)
+admin.site.register(Vehicle)
