fd6528526178c444375111b1ced6c93df5952381
GroSte <computer0205>
2016-06-20 09:28:40 +0200

    added simple user profile model

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
-from base.models import Mission, Vehicle
+from base.models import Mission, Vehicle, UserProfile
 
 admin.site.register(Mission, LeafletGeoAdmin)
 admin.site.register(Vehicle)
+admin.site.register(UserProfile)
--- base/models.py
+++ base/models.py
@@ -1,6 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from djgeojson.fields import PointField
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='user')
+    grade = models.CharField(max_length=20, blank=True, null=True,
+                             verbose_name=_('Grade'))
+    phone_number = models.CharField(max_length=25, blank=True, null=True,
+                                    verbose_name=_('Phone Number'))
+    mobile_phone_number = models.CharField(max_length=25, blank=True, null=True,
+                                    verbose_name=_('Mobile Phone Number'))
+    birth_date = models.DateField(max_length=45, blank=True, null=True,
+                                  verbose_name=_('Birth Date'))
+    online = models.BooleanField(default=False, verbose_name=_('Online'))
+    online_date = models.DateTimeField(blank=True, null=True, verbose_name=_('Online date'))
+    online_ip = models.CharField(max_length=15, blank=True, null=True, verbose_name=_('Online IP'))
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+    def __str__(self):
+        return '{0} {1}'.format(self.user.first_name, self.user.last_name)
 
 
 class Vehicle(models.Model):
@@ -17,18 +41,19 @@ class Vehicle(models.Model):
 
 
 class Mission(models.Model):
-    number = models.CharField(blank=True, null=True, max_length=20, verbose_name=_('Number')) # Einsatz-Nummer
-    alarm_time = models.DateTimeField(verbose_name=_('Alarm Time')) # Alarmierungszeit
-    keyword = models.CharField(max_length=100, verbose_name=_('Keyword'), db_index=True) # Stichwort
-    description = models.TextField(blank=True, null=True, verbose_name=_('Description')) # Beschreibung
-    volume = models.CharField(blank=True, null=True, max_length=100, verbose_name=_('Volume')) # Umfang, Groesse
-    concerned = models.CharField(blank=True, null=True, max_length=20, verbose_name=_('Concerned')) # Betroffene
-    name = models.CharField(blank=True, null=True, max_length=200, verbose_name=_('Name')) # Name
-    street = models.CharField(max_length=200, verbose_name=_('Street')) # Strasse
-    location = models.CharField(max_length=200, verbose_name=_('Location')) # Ort
-    coordinates = PointField(blank=True, null=True, verbose_name=_('Coordinates')) # Koordinaten
-    signal = models.BooleanField(default=True, verbose_name=_('Signal')) # Sondersignal
-    vehicles = models.ManyToManyField(Vehicle) # Fahrzeuge
+    number = models.CharField(blank=True, null=True, max_length=20, verbose_name=_('Number'))  # Einsatz-Nummer
+    alarm_time = models.DateTimeField(verbose_name=_('Alarm Time'))  # Alarmierungszeit
+    keyword = models.CharField(max_length=100, verbose_name=_('Keyword'), db_index=True)  # Stichwort
+    description = models.TextField(blank=True, null=True, verbose_name=_('Description'))  # Beschreibung
+    volume = models.CharField(blank=True, null=True, max_length=100, verbose_name=_('Volume'))  # Umfang, Groesse
+    concerned = models.CharField(blank=True, null=True, max_length=20, verbose_name=_('Concerned'))  # Betroffene
+    name = models.CharField(blank=True, null=True, max_length=200, verbose_name=_('Name'))  # Name
+    street = models.CharField(max_length=200, verbose_name=_('Street'))  # Strasse
+    location = models.CharField(max_length=200, verbose_name=_('Location'))  # Ort
+    coordinates = PointField(blank=True, null=True, verbose_name=_('Coordinates'))  # Koordinaten
+    signal = models.BooleanField(default=True, verbose_name=_('Signal'))  # Sondersignal
+    vehicles = models.ManyToManyField(Vehicle)  # Fahrzeuge
+    firefighters = models.ManyToManyField(UserProfile)  # Einsatzkraefte
 
     class Meta:
         verbose_name = _('Mission')
