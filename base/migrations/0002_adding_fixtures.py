1c2a19adcf48b095a47fe997459cd71f5281e14a
GroSte <computer0205>
2016-06-21 08:49:50 +0200

    combined migrations

---
+++ base/migrations/0002_adding_fixtures.py
@@ -0,0 +1,21 @@
+# encoding: utf8
+from django.db import migrations
+from base.utils import LoadDataWithModelState
+
+data_loader = LoadDataWithModelState('base_data.json')
+
+
+# create a new dump:
+# python manage.py dumpdata auth base contenttypes > base/fixtures/base_data.json
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=data_loader.load,
+            reverse_code=lambda: True,
+        ),
+    ]
