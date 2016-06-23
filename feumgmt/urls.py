68ea0870f639f7683891441941435f68816ce190
GroSte <computer0205>
2016-06-23 13:43:39 +0200

    added static file handling

--- feumgmt/urls.py
+++ feumgmt/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.views.i18n import javascript_catalog
@@ -17,4 +18,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT})
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
