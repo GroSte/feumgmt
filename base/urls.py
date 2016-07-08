f8d550153e9ca3a96d96d460eb7e582a7c49c066
GroSte <computer0205>
2016-07-08 09:25:44 +0200

    added url translations

--- base/urls.py
+++ base/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout_then_login
 from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 import views
 
 urlpatterns = [
     url(r'^login/$', login, {'template_name': 'base/login.html'}, name='login'),
     url(r'^logout/$', logout_then_login, {'login_url': reverse_lazy('login')}, name='logout'),
 
-    url(r'^dashboard/$', views.Dashboard.as_view(), name='dashboard'),
-    url(r'^user/password/?$', views.UserChangePassword.as_view(), name='user_change_password'),
-    url(r'^user-import/$', views.UserImport.as_view(), name='user_import'),
-    url(r'^message/?$', views.MessageList.as_view(), name='message_list'),
-    url(r'^message/new$', views.MessageCreate.as_view(), name='message_new'),
-    url(r'^message/(?P<pk>\d+)?/?$', views.MessageUpdate.as_view(), name='message_edit'),
-    url(r'^message/delete/(?P<pk>\d+)?/?$', views.MessageDelete.as_view(), name='message_delete'),
-    url(r'^gallery/?$', views.GalleryList.as_view(), name='gallery'),
+    url(_(r'^dashboard/$'), views.Dashboard.as_view(), name='dashboard'),
+    url(_(r'^user/password/?$'), views.UserChangePassword.as_view(), name='user_change_password'),
+    url(_(r'^user-import/$'), views.UserImport.as_view(), name='user_import'),
+    url(_(r'^message/?$'), views.MessageList.as_view(), name='message_list'),
+    url(_(r'^message/new$'), views.MessageCreate.as_view(), name='message_new'),
+    url(_(r'^message/(?P<pk>\d+)?/?$'), views.MessageUpdate.as_view(), name='message_edit'),
+    url(_(r'^message/delete/(?P<pk>\d+)?/?$'), views.MessageDelete.as_view(), name='message_delete'),
+    url(_(r'^gallery/?$'), views.GalleryList.as_view(), name='gallery'),
 ]
--- feumgmt/urls.py
+++ feumgmt/urls.py
@@ -14,8 +14,10 @@ js_info_dict = {
 urlpatterns = [
     url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='js_i18n'),
     url(r'^$', RedirectView.as_view(pattern_name='dashboard', permanent=True)),
-    url(r'^base/', include('base.urls')),
-    url(r'^event/', include('event.urls')),
+    # url(r'^base/', include('base.urls')),
+    # url(r'^event/', include('event.urls')),
+    url(r'^basis/', include('base.urls')),
+    url(r'^ereignis/', include('event.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
